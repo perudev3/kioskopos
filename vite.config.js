56dfde01cbe44d8b1df44022e37ba66ee15aa451
perudev3 <peru.dev3@gmail.com>
2026-02-14 20:48:19 -0500

    hacemos configuraciones finales en la app para ser instalable

--- vite.config.js
+++ vite.config.js
@@ -9,26 +9,32 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico'],
+    
       manifest: {
-        name: 'Kiosko POS',
-        short_name: 'KioskoPOS',
-        description: 'Sistema de ventas y control de stock para kioskos',
+        name: 'KioPOS',
+        short_name: 'KioPOS',
+        description: 'Sistema de ventas y control de comercios como Tiendas y Kioskos',
         theme_color: '#4f46e5',
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/',
+        scope: '/',
         icons: [
           {
-            src: '/public/logo-sin-fondo.png',
+            src: '/logo-sin-fondo.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/public/logo-sin-fondo.png',
+            src: '/logo-sin-fondo.png',
             sizes: '512x512',
             type: 'image/png',
           },
         ],
+      },
+    
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
       },
     }),
   ],
